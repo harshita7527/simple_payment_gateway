04754ed318ae95c0c53601e591bebb44aeee8d6c
harshita <harshita75rose@gmail.com>
2021-11-14 17:35:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Payment-Gateway-Integration
+
+Project developed during Internship at The Sparks Foundation (#task3)
+
+Simple website where payment gateway is integrated from Rajorpay. There is a simple donate button which loads the payment page where the user can select amount to be paid and the payment type. Once payment is done an invoice will be generated and email will be sent to the donar. 
+
+Built using : HTML, CSS, Bootstrap and Javascript
---
+++ script.js
@@ -0,0 +1,29 @@
+
+    function validation()
+    {
+     var name=document.getElementById("name").value;
+     var email=document.getElementById("email").value;
+     var subject=document.getElementById("subject").value;
+     var message=document.getElementById("message").value;
+     
+     if(name=="")
+     {
+       alert("Please Enter Name");
+     }
+     else if(email=="")
+     {
+       alert("Please Enter Email Id");
+     }
+     else if(subject=="")
+     {
+       alert("Please Enter subject");
+     }
+     else if(message=="")
+     {
+       alert("Please Write Message");
+     }
+     else 
+     {
+       alert("Message Send Successfully");
+     }
+    }
